252aca49c1cbaf75869aafaa03e3876c18c11f8e
yuncongli <yuncongli@tencent.com>
2023-04-01 00:49:15 +0800

    my alpaca colab

---
+++ common/common_path.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+project_dir = '/my-alpaca/MyDrive/my-alpaca'
+
+alpaca_lora_dir = os.path.join(project_dir, 'alpaca_lora')
+
+if __name__ == '__main__':
+    print(project_dir)
