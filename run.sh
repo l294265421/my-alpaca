c16c8f28690236acebb409ca09dab8784df89332
yuncongli <yuncongli@tencent.com>
2023-04-02 00:09:55 +0800

    automl

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# nohup sh run.sh my_alpaca/my_alpaca_autodl/finetune.py > autodl.log 2>&1 &
+# nohup sh run.sh my_alpaca/autodl/finetune.py > autodl.log 2>&1 &
 start_time=`date +%Y%m%d%H%M%S`
 echo "start ${start_time}--------------------------------------------------"
 
