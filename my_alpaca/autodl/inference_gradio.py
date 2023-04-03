9bc62641f5bb223eaf154d663991e011c127ef90
yuncongli <yuncongli@tencent.com>
2023-04-04 00:57:58 +0800

    fix gradio

--- my_alpaca/autodl/inference_gradio.py
+++ my_alpaca/autodl/inference_gradio.py
@@ -25,7 +25,7 @@ except:  # noqa: E722
 def main(
     load_8bit: bool = False,
     base_model: str = "decapoda-research/llama-7b-hf",
-    lora_weights: str = "tloen/alpaca-lora-7b",
+    lora_weights: str = "./lora-alpaca",
     prompt_template: str = "",  # The prompt template to use, will default to alpaca.
     server_name: str = "0.0.0.0",  # Allows to listen on all interfaces by providing '0.
     share_gradio: bool = True,
